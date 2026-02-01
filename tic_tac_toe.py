f83d94fe99defc6aa65057a116f5a10cf0f37379
Hung-Chi970104 <hungchi970104@gmail.com>
2026-02-01 10:56:47 -0500

    Finished coding gamee logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -53,6 +53,91 @@ player_1_LEDs[location].value(1)
 print(player_1_LEDs)
 print(player_2_LEDs)
 
+class CHECK_GRID:
+    
+    # Player Won
+    @staticmethod
+    def player_won(player, coordinates):
+        print(f"player {player} won")
+        if player == 1:
+            for i in range(3):
+                for led in coordinates:
+                    player_1_LEDs[led].value(0)
+                utime.sleep(0.3)
+                for led in coordinates:
+                    player_1_LEDs[led].value(1)
+                utime.sleep(0.3)
+        elif player == 2:
+            for i in range(3):
+                for led in coordinates:
+                    player_2_LEDs[led].value(0)
+                utime.sleep(0.3)
+                for led in coordinates:
+                    player_2_LEDs[led].value(1)
+                utime.sleep(0.3)
+        # Draw
+        else:
+            for i in range(3):
+                for led in range(8):
+                    player_1_LEDs[led].value(0)
+                    player_2_LEDs[led].value(0)
+                utime.sleep(0.3)
+                for led in range(8):
+                    player_1_LEDs[led].value(1)
+                    player_2_LEDs[led].value(1)
+                utime.sleep(0.3)
+            
+        # Reset
+        global location, current_player, grid
+        location = 0
+        current_player = 1
+        grid = [
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0
+            ]
+        for i in range(9):
+            player_1_LEDs[i].value(0)
+            player_2_LEDs[i].value(0)
+
+    # Columns (winning condition = 0)
+    @staticmethod
+    def check_column():
+        for i in range(3):
+            if grid[i] == grid[i+3] == grid[i+6]:
+                coordinates = [i, i+3, i+6]
+                if grid[i] == 1:
+                    CHECK_GRID.player_won(1, coordinates)
+                elif grid[i] == 2:
+                    CHECK_GRID.player_won(2, coordinates)
+                    
+    # Rows (winning condition = 1)
+    @staticmethod
+    def check_row():
+        for i in range(3):
+            if grid[i*3] == grid[i*3+1] == grid[i*3+2]:
+                coordinates = [i*3, i*3+1, i*3+2]
+                if grid[i*3] == 1:
+                    CHECK_GRID.player_won(1, coordinates)
+                elif grid[i*3] == 2:
+                    CHECK_GRID.player_won(2, coordinates)
+                    
+    # Diagonal (winning condition = 2)
+    @staticmethod
+    def check_diagonal():
+        leftTop_rightBottom = grid[0] == grid[4] == grid[8]
+        rightTop_leftBottom = grid[2] == grid[4] == grid[6]
+        coordinates = []
+        if leftTop_rightBottom or rightTop_leftBottom:
+            if leftTop_rightBottom:
+                coordinates = [0, 4, 8]
+            else:
+                coordinates = [2, 4, 6]
+            if grid[4] == 1:
+                CHECK_GRID.player_won(1, coordinates)
+            elif grid[4] == 2:
+                CHECK_GRID.player_won(2, coordinates)
+                
 while True:
     # Press left button for next
     if button_left.value() == 0:
@@ -81,6 +166,14 @@ while True:
         print(grid)
         while button_right.value() == 0:
             utime.sleep_ms(1)
+            
+        CHECK_GRID.check_column()
+        CHECK_GRID.check_row()
+        CHECK_GRID.check_diagonal()
+        print(grid)
+        
+        if 0 not in grid:
+            CHECK_GRID.player_won(0, [])
         
     # Control selected LED
     if current_player == 1:
@@ -102,5 +195,3 @@ while True:
             player_1_LEDs[led].value(1)
         elif grid[led] == 2:
             player_2_LEDs[led].value(1)
-            
-    ### GAME LOGIC
